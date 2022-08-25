0de2b20aa52e6dd42a7dfa6dafaaba718bc5c164
user.email <aromito7@gmail.com>
2022-08-25 11:28:17 -0400

    Phase 1: Added an API router.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,7 @@
 // backend/routes/index.js
 const express = require('express');
 const router = express.Router();
-
+const apiRouter = require('./api');
 /*
 router.get('/hello/world', function(req, res) {
   res.cookie('XSRF-TOKEN', req.csrfToken());
@@ -9,8 +9,6 @@ router.get('/hello/world', function(req, res) {
 });
 */
 
-// backend/routes/index.js
-// ...
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
   const csrfToken = req.csrfToken();
@@ -19,5 +17,8 @@ router.get("/api/csrf/restore", (req, res) => {
     'XSRF-Token': csrfToken
   });
 });
+
+
+router.use('/api', apiRouter);
 
 module.exports = router;
