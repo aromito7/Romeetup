94ca82492c0406fba7fb8d1034829b19dba80122
user.email <aromito7@gmail.com>
2022-09-01 07:28:15 -0400

    Fixed the output format of get-session.

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -49,8 +49,8 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     toSafeObject() {
-      const { id, username, email } = this; // context will be the User instance
-      return { id, username, email };
+      const { id, firstName, lastName, email } = this; // context will be the User instance
+      return { id, firstName, lastName, email };
     }
   };
 
