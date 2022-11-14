f1817189fe7dfd619455a01b5bea54d80c123c36
user.email <aromito7@gmail.com>
2022-11-14 11:40:52 -0500

    Finished loading page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import Homepage from './components/Homepage';
 import Navigation from './components/Navigation';
+import Login from './components/Login'
 import topRightImage from './images/top-right-blob.png';
 import centerLeftImage from './images/center-left-blob.png';
 import centerImage from './images/center-blob.png';
+import { Route, Switch } from 'react-router-dom';
 import './css/App.css';
 
 function App() {
@@ -12,8 +14,19 @@ function App() {
         <img src={topRightImage} id='top-right-blob' className='blob'/>
         <Navigation/>
         <div id='main'>
+
           <img src={centerImage} id='center-blob' className='blob'/>
-          <Homepage/>
+          <Switch>
+            <Route exact path="/">
+              <Homepage/>
+            </Route>
+            <Route path="/login">
+              <Login/>
+            </Route>
+            <Route path="/">
+              Error 404
+            </Route>
+          </Switch>
         </div>
       </div>
   );
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,12 @@
+export default () => (
+    <div id='login'>
+        <div>
+            <label id="username">Username</label>
+            <input id="username"></input>
+        </div>
+        <div>
+            <label id="password">Password</label>
+            <input id="password"></input>
+        </div>
+    </div>
+)
