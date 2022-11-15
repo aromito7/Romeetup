abf68bcf4347d3f4f2ef6010cb9b0a6580e2926e
user.email <aromito7@gmail.com>
2022-11-15 11:27:39 -0500

    Updated sign in page.

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -1,5 +1,5 @@
 // frontend/src/components/SignupFormPage/index.js
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
@@ -8,32 +8,78 @@ import './SignupForm.css'
 function SignupFormPage() {
   const dispatch = useDispatch();
   const sessionUser = useSelector((state) => state.session.user);
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [hasSubmitted, setHasSubmitted] = useState(false);
   const [errors, setErrors] = useState([]);
-
-  if (sessionUser) return <Redirect to="/" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (password === confirmPassword) {
-      setErrors([]);
-      return dispatch(sessionActions.signup({ email, username, password }))
+    setHasSubmitted(true)
+    console.log("Hello")
+    if(!errors){
+      const newUser = {
+        firstName,
+        lastName,
+        email,
+        username,
+        password
+      }
+      
+      return dispatch(sessionActions.signup(newUser))
         .catch(async (res) => {
           const data = await res.json();
           if (data && data.errors) setErrors(data.errors);
         });
     }
-    return setErrors(['Confirm Password field must be the same as the Password field']);
+
   };
+
+  useEffect(() => {
+    const validationErrors = []
+    if(!firstName) validationErrors.push("First Name is required")
+    if(!lastName) validationErrors.push("Last Name is required")
+    if(!email) validationErrors.push("Email is required")
+    if(!username) validationErrors.push("Username is required")
+    if(!password) validationErrors.push("Please enter a password")
+    if(!confirmPassword)  validationErrors.push("Please confirm password")
+    if(password && confirmPassword && (password !== confirmPassword)){
+      validationErrors.push("Passwords do not match")
+    }
+    setErrors(validationErrors)
+  },[firstName, lastName, email, username, password, confirmPassword])
+
+  if (sessionUser) return <Redirect to="/" />;
 
   return (
     <form onSubmit={handleSubmit} id='signup'>
+      {hasSubmitted &&
       <ul>
         {errors.map((error, idx) => <li key={idx}>{error}</li>)}
       </ul>
+      }
+      <label>
+        First Name
+        <input
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Last Name
+        <input
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+          required
+        />
+      </label>
       <label>
         Email
         <input
@@ -70,7 +116,7 @@ function SignupFormPage() {
           required
         />
       </label>
-      <button type="submit">Sign Up</button>
+      <button type="submit" formNoValidate="true">Sign Up</button>
     </form>
   );
 }
